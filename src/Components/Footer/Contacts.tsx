ddf30acd682174663e7bc976381cf7ef5ba18ea8
Alexander Kuznecov <alexander.kuznecov16@gmail.com>
2024-02-18 16:03:50 +0200

    id re

--- src/Components/Footer/Contacts.tsx
+++ src/Components/Footer/Contacts.tsx
@@ -4,7 +4,7 @@ import { IoMdSend } from "react-icons/io";
 
 const Contacts: React.FC = () => {
   return (
-    <div id="Contacts" className="Contacts">
+    <div id="Contact" className="Contacts">
        <div className="container">
         <h3 className="Contacts-name section-name">SAY <span>HE</span>LLO!</h3>
         <form action='/'>
